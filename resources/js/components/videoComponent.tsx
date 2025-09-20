37e8aeaf15a81b94ac32a8d8f4fbcc630dc8c07d
mike <mikes.yamauchi@gmail.com>
2025-09-20 11:31:59 -0700

    made code reusable

--- resources/js/components/videoComponent.tsx
+++ resources/js/components/videoComponent.tsx
@@ -1,4 +1,4 @@
-
+import React from 'react'; // Make sure to import React
 import {
   Accordion,
   AccordionSummary,
@@ -6,33 +6,58 @@ import {
   Typography,
 } from '@mui/material';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-export default function VideoComponent({ youtubeId, startTime, timeStamp, setTimestamp }: { youtubeId: string; startTime: number; timeStamp: any, setTimestamp: any }) {
-    console.log('Rendering VideoComponent with props:', { youtubeId, startTime, timeStamp });
-    return(
-     <Accordion>
 
-       <AccordionSummary
-         expandIcon={<ArrowDropDownIcon />}
-         aria-controls="panel1a-content"
-         id="panel1a-header"
-       >
-         <Typography component="span">{youtubeId}</Typography>
-       </AccordionSummary>
+// Define the shape of your props for better type safety
+interface Timestamp {
+  id: number;
+  label: string;
+  timestamp_seconds: number;
+}
 
-       <AccordionDetails>
-         <iframe width="560" height="315" src={`https://www.youtube.com/embed/${youtubeId}&start=${startTime}`} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+interface VideoComponentProps {
+  youtubeId: string;
+  startTime: number;
+  timeStamp: Timestamp[];
+  setTimestamp: (time: number) => void;
+}
 
-            <div>
-                {timeStamp.map((timestamp: { id: string; label: string; time: number }) => (
-                    <button key={timestamp.id} onClick={() => setTimestamp(timestamp.time)}>
-                        {timestamp.label} - {timestamp.time}
-                    </button>
-                ))}
-            </div>
-       </AccordionDetails>
+export default function VideoComponent({ youtubeId, startTime, timeStamp, setTimestamp }: VideoComponentProps) {
+  return (
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ArrowDropDownIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography component="span">{youtubeId}</Typography>
+      </AccordionSummary>
 
+      <AccordionDetails>
+        <iframe
+          // FIX 1: Add a dynamic key. This is the most important change!
+          key={`${youtubeId}-${startTime}`}
+          width="560"
+          height="315"
+          // FIX 2: Use '?' for the first parameter and add autoplay for better UX
+          src={`https://www.youtube.com/embed/${youtubeId}?start=${startTime}&autoplay=1`}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
 
-
-     </Accordion>
-    );
+        <div>
+          {timeStamp.map((timestamp) => (
+            <button
+              key={timestamp.id}
+              // FIX 3: Use the correct property 'timestamp_seconds'
+              onClick={() => setTimestamp(timestamp.timestamp_seconds)}
+            >
+              {timestamp.label} - {timestamp.timestamp_seconds}s
+            </button>
+          ))}
+        </div>
+      </AccordionDetails>
+    </Accordion>
+  );
 }
