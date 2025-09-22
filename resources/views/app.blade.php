4546857c198acd6d0f600d13680b185659dbb988
mike <mikes.yamauchi@gmail.com>
2025-09-22 15:03:49 -0700

    changed styling

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Get Lost') }}</title>
+        <title inertia>Get Lost Timestamps</title>
+        <meta name="description" content="Find timestamps from your favorite analog roadtrip series. Get Lost.">
 
         <!-- Favicon links comment-->
         <link rel="icon" href="/favicon.ico" sizes="any">
